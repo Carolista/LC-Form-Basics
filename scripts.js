1df1efca9f941761f80508a21ceb6878045630b4
Carolista <cjones.renaissancewoman@gmail.com>
2021-03-04 16:31:59 -0600

    improved comments throughout DOM code

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ function getId() {
     return nextId;
 }
 
+// Each entry is an object based on this class
 class Entry {
     constructor(day, category, activity) {
         this.id = getId();
@@ -19,6 +20,7 @@ class Entry {
     }
 }
 
+// Assign a different color for the entry labels, based on category
 let colors = {
     cooking: "#c04e3a",
     exercise: "#e4823c",
@@ -44,28 +46,35 @@ function init() {
 
     let entries = []; // to hold Entry-class objects
 
-    // Create objects for specific form elements
+    // Create DOM objects for specific form elements
     let day = document.getElementById("day");
     let category = document.getElementById("category");
     let activity = document.getElementById("activity");
     let create = document.getElementById("create");
     let newEntry = document.getElementById("new-entry");
-    let form = document.getElementById("form");
 
     function addNewEntry() {
-        // Store data
+
+        // Store data in new object based on Entry class
         let newEntry = new Entry(day.value, category.value, activity.value);
         entries.push(newEntry);
-        // Display on page
+
+        // Set which day section to use in HTML template
         let dayList = document.getElementById(newEntry.day + "-list");
+
+        // Add day header for the first time if no previous entries for this day
         if (dayList.innerHTML === "") {
             dayList.innerHTML += `
                 <div class="day-header">${newEntry.day[0].toUpperCase() + newEntry.day.slice(1)}</div>
             `
         }
+
+        // Add entry to day section on page
         dayList.innerHTML += `
             <p class="entry"><span class="dot" id="dot${newEntry.id}">${newEntry.category}</span> ${newEntry.activity}</p>
         `
+
+        // Assign colored tag to specific entry based on its category
         document.getElementById("dot" + newEntry.id).style.backgroundColor = colors[newEntry.category];
     }
 
@@ -75,27 +84,29 @@ function init() {
         activity.value = "";
     }
 
-    // Non-form click events
+    // Click events using event delegation
     document.addEventListener("click", function(event) {
 
+        // Create New Entry button
         if (event.target.id === "go-to-form") {
             create.style.visibility = "hidden";
             newEntry.style.visibility = "visible";
             event.preventDefault(); // ensure this is the only button registering for the click
         }
 
+        // Cancel button
         if (event.target.id === "cancel") {
             create.style.visibility = "visible";
             newEntry.style.visibility = "hidden";
             event.preventDefault(); // ensure this is the only button registering for the click
         }
 
+        // Submit button
         if (event.target.id === "submit") {
-             // Validate no empty inputs
+             // Validate to ensure no empty input fields
             if (day.value === "" || category.value === "" || activity.value === "") {
                 alert("All fields required to create a new entry.")
-                console.log("not all fields are valid");  
-            } else {
+            } else { // Create new entry and reset form
                 addNewEntry();
                 clearForm();
                 create.style.visibility = "visible";
